13ec90918567b6ff6707184dee64ccedff393ab8
PSGS99 <pgonzalez4756@uta.edu.ec>
2022-01-10 20:29:59 -0500

    Paul Gonzalez 20:30

--- app/src/main/java/com/gonzalezp/apps/androidfunwithflags/Activity_Login.java
+++ app/src/main/java/com/gonzalezp/apps/androidfunwithflags/Activity_Login.java
@@ -9,14 +9,25 @@ import android.widget.Button;
 public class Activity_Login extends AppCompatActivity {
 
     public Button boton;
+    public Button boton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        boton = findViewById(R.id.button);
+        boton = findViewById(R.id.button2);
         boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        });
+
+        boton2 = findViewById(R.id.button);
+        boton2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
