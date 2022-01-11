d3c7c393758224af893d17300623e373abddd52f
PSGS99 <pgonzalez4756@uta.edu.ec>
2022-01-10 20:20:09 -0500

    Paul Gonzalez 20:15

--- app/src/main/java/com/gonzalezp/apps/androidfunwithflags/Activity_Login.java
+++ app/src/main/java/com/gonzalezp/apps/androidfunwithflags/Activity_Login.java
@@ -1,13 +1,29 @@
 package com.gonzalezp.apps.androidfunwithflags;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Login extends AppCompatActivity {
+
+    public Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        boton = findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/gonzalezp/apps/androidfunwithflags/psgs_loginActivity.java
+++ app/src/main/java/com/gonzalezp/apps/androidfunwithflags/psgs_loginActivity.java
@@ -1,5 +1,22 @@
 package com.gonzalezp.apps.androidfunwithflags;
 
-public class psgs_loginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+public class psgs_loginActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.psgs);
+
+        Button next = (Button) findViewById(R.id.Button01);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Activity2.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+    }
 }
